e70a95071f313c6b547d7c7d6cd27c9f9ac4b782
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-10-29 00:20:47 +0600

    added react router and header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
 import './App.css';
+import Header from './components/Header/Header';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Services from './components/Services/Services';
+import Client from './components/Client/Client';
+import MyOrders from './components/MyOrders/MyOrders';
+import ManageOrders from './components/ManageOrders/ManageOrders';
+import AddServices from './components/AddServices/AddServices';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div>
-
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+          <Route path='/home'>
+            <Home></Home>
+          </Route>
+          <Route path='/about'>
+            <About></About>
+          </Route>
+          <Route path='/services'>
+            <Services></Services>
+          </Route>
+          <Route path='/client'>
+            <Client></Client>
+          </Route>
+          <Route path='/myOrders'>
+            <MyOrders></MyOrders>
+          </Route>
+          <Route path='/manageOrders'>
+            <ManageOrders></ManageOrders>
+          </Route>
+          <Route path='/addServices'>
+            <AddServices></AddServices>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,94 @@
 import React from 'react';
+import logo from '../../images/logo.png'
+import { Container, Nav, Navbar, Stack } from 'react-bootstrap';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
+            <Navbar collapseOnSelect navbar-collapse expand="lg" bg="light" variant="light">
+                <Container>
+                    <Link to='/'> <Navbar.Brand><img src={logo} alt="" /></Navbar.Brand></Link>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Stack direction="horizontal" gap={3}>
+                                <NavLink
+                                    className='text-decoration-none fs-5 text-warning ms-2'
+                                    to="/home"
+                                    activeStyle={{
+                                        fontWeight: "bold",
+                                        color: "red"
+                                    }}
+                                >
+                                    Home
+                                </NavLink>
+                                <NavLink
+                                    className='text-decoration-none fs-5 text-warning ms-2'
+                                    to="/about"
+                                    activeStyle={{
+                                        fontWeight: "bold",
+                                        color: "red"
+                                    }}
+                                >
+                                    About Us
+                                </NavLink>
+                                <NavLink
+                                    className='text-decoration-none fs-5 text-warning ms-2'
+                                    to="/services"
+                                    activeStyle={{
+                                        fontWeight: "bold",
+                                        color: "red"
+                                    }}
+                                >
+                                    Services
+                                </NavLink>
+                                <NavLink
+                                    className='text-decoration-none fs-5 text-warning ms-2'
+                                    to="/client"
+                                    activeStyle={{
+                                        fontWeight: "bold",
+                                        color: "red"
+                                    }}
+                                >
+                                    Client Review
+                                </NavLink>
+                                <NavLink
+                                    className='text-decoration-none fs-5 text-warning ms-2'
+                                    to="/myOrders"
+                                    activeStyle={{
+                                        fontWeight: "bold",
+                                        color: "red"
+                                    }}
+                                >
+                                    My Orders
+                                </NavLink>
+                                <NavLink
+                                    className='text-decoration-none fs-5 text-warning ms-2'
+                                    to="/manageOrders"
+                                    activeStyle={{
+                                        fontWeight: "bold",
+                                        color: "red"
+                                    }}
+                                >
+                                    Manage All orders
+                                </NavLink>
+                                <NavLink
+                                    className='text-decoration-none fs-5 text-warning ms-2'
+                                    to="/addServices"
+                                    activeStyle={{
+                                        fontWeight: "bold",
+                                        color: "red"
+                                    }}
+                                >
+                                    Add New Services
+                                </NavLink>
+                            </Stack>
+                        </Nav>
 
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
