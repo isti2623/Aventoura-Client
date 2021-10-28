f379e6c6784d0fb62ec95e68963c55e6eba38a10
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-10-29 02:20:40 +0600

    added services

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
     return (
-        <div>
+        <div className='container py-5'>
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+                {
+                    services.map(service =>
+                        <div className="col">
+                            <div className="card h-100">
+                                <img src={service.image} className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">{service.title}</h5>
+                                    <p className="card-text">{service.desc}</p>
+                                </div>
+                                <div className="card-footer">
+                                    <div className="btn btn-warning">BOOK NOW</div>
+                                </div>
+                            </div>
+                        </div>
 
+
+                    )
+
+                }
+
+
+            </div>
         </div>
     );
 };
