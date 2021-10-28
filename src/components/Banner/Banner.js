3f6d27abc3087c680e3e36ed61e16060b5845504
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-10-29 00:39:27 +0600

    updated banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -12,7 +12,7 @@ const Banner = () => {
                         <div className='text-part'>
                             <h4 className='text-warning fs-2 ms-1'>WELCOME TO AVENTOURA</h4>
                             <h1 className='text-white ms-1 custom-h1'>Explore new worlds with us</h1>
-                            <p className='text-white fw-bold ms-1'>Tourism is the activities of people traveling to and staying in places outside their usual environment for leisure, business or other purposes for not more than one consecutive year.</p>
+                            <p className='text-white ms-1'>Tourism is the activities of people traveling to and staying in places outside their usual environment for leisure, business or other purposes for not more than one consecutive year.</p>
                             <Link to='services'> <Button className='mt-3 rounded ' variant="warning fw-bold">OUR SERVICES</Button></Link>
                         </div>
                     </div>
