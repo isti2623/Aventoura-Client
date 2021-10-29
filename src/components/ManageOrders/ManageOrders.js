c5f570db5f1e80fd6ad3ffb7ecc2f0f274db8d6b
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-10-30 00:19:59 +0600

    final update

--- src/components/ManageOrders/ManageOrders.js
+++ src/components/ManageOrders/ManageOrders.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
 
 const ManageOrders = () => {
-    return (
-        <div>
+    const [order, setOrder] = useState([]);
 
+    useEffect(() => {
+        fetch("http://localhost:5000/orders")
+            .then((res) => res.json())
+            .then((data) => setOrder(data));
+    }, []);
+    return (
+        <div className='container'>
+            <h1 className='text-center my-3'>All Orders: {order?.length}</h1>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Address</th>
+                    </tr>
+                </thead>
+                {order?.map((pd, index) => (
+                    <tbody>
+                        <tr>
+                            <td>{index}</td>
+                            <td>{pd?.name}</td>
+                            <td>{pd?.email}</td>
+                            <td>{pd?.phone}</td>
+                            <td>{pd?.address}</td>
+                            <button className="btn bg-warning p-2">Delete</button>
+                        </tr>
+                    </tbody>
+                ))}
+            </Table>
         </div>
     );
 };
--- src/components/MyOrders/MyOrders.js
+++ src/components/MyOrders/MyOrders.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { Table } from 'react-bootstrap';
+import useAuth from '../../hooks/useAuth';
 
 const MyOrders = () => {
-    const [order, setOrder] = useState([]);
-
+    const { user } = useAuth();
+    const [events, setEvents] = useState([]);
     useEffect(() => {
-        fetch("http://localhost:5000/orders")
+        fetch(`http://localhost:5000/myOrders/${user?.email}`)
             .then((res) => res.json())
-            .then((data) => setOrder(data));
-    }, []);
+            .then((data) => setEvents(data));
+    }, [user.email]);
 
+    console.log(events);
+    console.log(events);
     return (
         <div className='container'>
-            <h1 className='text-center my-3'>Your All Orders: {order?.length}</h1>
+            <h1 className='text-center my-3'>All Orders: {events?.length}</h1>
             <Table striped bordered hover>
                 <thead>
                     <tr>
@@ -23,7 +26,7 @@ const MyOrders = () => {
                         <th>Address</th>
                     </tr>
                 </thead>
-                {order?.map((pd, index) => (
+                {events?.map((pd, index) => (
                     <tbody>
                         <tr>
                             <td>{index}</td>
@@ -39,5 +42,4 @@ const MyOrders = () => {
         </div>
     );
 };
-
 export default MyOrders;
