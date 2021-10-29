55b6a8649ce0893d415e99d6f9cb09e7b937c5c1
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-10-29 23:15:13 +0600

    added

--- src/components/ServiceDetails/ServiceDetails.js
+++ src/components/ServiceDetails/ServiceDetails.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
 
 const ServiceDetails = () => {
     const { serviceId } = useParams();
@@ -10,7 +12,37 @@ const ServiceDetails = () => {
             .then(data => setServ(data))
     }, [])
 
-    const findServ = serv.find(tour => tour._id == serviceId)
+    const findServ = serv.find(tour => tour._id == serviceId);
+
+
+    /*  form Part */
+
+    const { user } = useAuth();
+    const { register, reset, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+        data.email = user.email;
+        data.name = user.displayName;
+        data.serviceId = findServ?.title;
+        data.serviceId = findServ?.image;
+
+
+        console.log(data);
+        fetch("http://localhost:5000/orders", {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(data),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data) {
+                    alert('added successfully');
+                    reset();
+                }
+            });
+    };
+
+
     return (
         <div className='container'>
             <div className="row container">
@@ -22,12 +54,31 @@ const ServiceDetails = () => {
                     <h1 className='mt-5 fw-bold text-warning'>{findServ?.title}</h1>
                     <hr />
                     <h5 className='mt-2'>{findServ?.desc}</h5>
-                    <div className="btn btn-warning fw-bold mt-3">PLACE ORDER</div>
-
 
 
                 </div>
             </div>
+
+
+
+            {/* Form */}
+            <div className='form-part'>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    {/* register your input into the hook by invoking the "register" function */}
+                    <input required className='p-2 m-2' placeholder='address' {...register("address")} />
+                    <br />
+
+                    {/* include validation with required or other standard HTML validation rules */}
+                    <input required className='p-2 m-2' placeholder='phone number' {...register("phone")} />
+                    <br />
+                    {/* errors will return when field validation fails  */}
+                    {errors.exampleRequired && <span>This field is required</span>}
+                    <br />
+                    <input className='className="btn btn-warning fw-bold mt-3"' value='PLACE ORDER' type="submit" />
+                </form>
+            </div>
+
+
 
         </div>
     );
